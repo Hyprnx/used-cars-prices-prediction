d0ee2b5afae0b4bf008e59f46299309f5154cb9a
Hyprnx <toducanh2001@gmail.com>
2022-04-19 17:45:33 +0700

    update pycache file

--- crawlers/SanXeHotCrawler.py
+++ crawlers/SanXeHotCrawler.py
@@ -48,17 +48,19 @@ class ExtractSanXeTotLink(BaseClass):
 class SanXeHotCrawler(BaseClass):
     def __init__(self):
         super().__init__()
-        self.df = pd.read_json(path + '/sanxehot_links.json').T
+        file_path = path + '/sanxehot_links.json'
+        self.df = pd.read_json(file_path).T
         self.car_df = pd.DataFrame(columns=['name', 'brand', 'source_url', 'type', 'origin', 'km_driven',
                                             'external_color', 'seats', 'engine_capacity', 'fuels',
                                             'transmission', 'wheel_drive', 'price', 'year'])
+        self.requests_session = requests.session()
 
     def crawl(self):
         for i in range(0, len(self.df)):
             url = self.df.iloc[i]['url']
             type = self.df.iloc[i]['type']
             self.log.info('Crawling %s', url)
-            req = requests.get(url, headers=HEADERS).text
+            req = self.requests_session.get(url, headers=HEADERS).text
             soup = BeautifulSoup(req, 'lxml')
             body_page = soup.find('table', class_='info').tbody
             car_feature = body_page.find_all('td')
@@ -85,13 +87,15 @@ class SanXeHotCrawler(BaseClass):
             origin = car_feature[12].text
             external_color = car_feature[14].text
 
-            self.car_df = self.car_df.append(pd.Series({'name': name, 'brand': None, 'source_url': url, 'type': type,
+            car_info = pd.Series({'name': name, 'brand': None, 'source_url': url, 'type': type,
                                                         'origin': origin, 'km_driven': km_driven,
                                                         'external_color': external_color,
                                                         'seats': None, 'engine_capacity': engine_capacity,
                                                         'fuels': fuels,
                                                         'transmission': transmission, 'wheel_drive': None,
-                                                        'price': price, 'year': year}), ignore_index=True)
+                                                        'price': price, 'year': year})
+
+            self.car_df = self.car_df.append(car_info, ignore_index=True)
 
         self.car_df.to_csv(path + '/sanxehot_detail.csv')
 
